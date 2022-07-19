e713c66c70677415bfdadec6b072738bb5430152
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-07-19 13:37:47 -0600

    Updated index
    created message variable

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 let firstCard = 11;
-let secondCard = 10;
+let secondCard = 7;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
 if (sum <= 20) {
-  console.log("Do you want to draw a new card?");
+  message = "Do you want to draw a new card?";
 } else if (sum === 21) {
-  console.log("WoHoo!! You have Blackjack!");
+  message = "WoHoo!! You have Blackjack!";
   hasBlackJack = true;
 } else {
-  console.log("You're out of the game!");
+  message = "You're out of the game!";
+  isAlive = false;
 }
-
-//CASH OUT
-console.log(hasBlackJack);

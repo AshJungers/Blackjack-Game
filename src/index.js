58cc6a76674756a2399be0e4b21022c1584dbffd
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-07-19 15:36:55 -0600

    Created onclick button & function

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "WoHoo!! You have Blackjack!";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game!";
-  isAlive = false;
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "WoHoo!! You have Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  console.log(message);
 }

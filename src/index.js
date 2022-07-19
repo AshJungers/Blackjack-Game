5dab47c958101187a3418c0706f3aed851366b6d
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-07-19 13:14:53 -0600

    added true/false to keep track of the data/state of the game

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 let firstCard = 11;
-let secondCard = 11;
+let secondCard = 10;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
 
 if (sum <= 20) {
   console.log("Do you want to draw a new card?");
 } else if (sum === 21) {
   console.log("WoHoo!! You have Blackjack!");
+  hasBlackJack = true;
 } else {
   console.log("You're out of the game!");
 }
+
+//CASH OUT
+console.log(hasBlackJack);
